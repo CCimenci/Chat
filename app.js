245b68f182728ab085790f0bf844ccc93517f9d5
CCimenci <cagricimenci@gmail.com>
2018-12-26 18:28:07 +0300

    redis installed

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ const chat=require('./routes/chat');
 
 
 const app = express();
+
+//helpers
 const db = require ('./helpers/db')();
+
+//middleware
+const isAuthenticated = require('./middleware/isAuthenticated');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,7 +44,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true , maxAge: 14 * 24 * 3600000  }
+  cookie: {  maxAge: 14 * 24 * 3600000  }
 }));
 
 
@@ -49,7 +54,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/auth', auth);
-app.use('/chat',chat);
+app.use('/chat',isAuthenticated,chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
